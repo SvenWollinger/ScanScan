b1450c882ab7df6d85ffcbdc4ef7acf6507aec0a
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-11-08 15:30:32 +0100

    Initial commit

---
+++ src/main/java/io/wollinger/ScanScan.java
@@ -0,0 +1,87 @@
+package io.wollinger;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ScanScan {
+    private static Scanner scanner = null;
+
+    private static void ensureScanner() {
+        if(scanner == null)
+            scanner = new Scanner(System.in);
+    }
+
+    public static String nextLine() {
+        ensureScanner();
+        return scanner.nextLine();
+    }
+
+    public static String next() {
+        ensureScanner();
+        return scanner.next();
+    }
+
+    public static int nextInt() {
+        ensureScanner();
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static short nextShort() {
+        ensureScanner();
+        short input = scanner.nextShort();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static boolean nextBoolean() {
+        ensureScanner();
+        boolean input = scanner.nextBoolean();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static float nextFloat() {
+        ensureScanner();
+        float input = scanner.nextFloat();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static double nextDouble() {
+        ensureScanner();
+        double input = scanner.nextDouble();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static long nextLong() {
+        ensureScanner();
+        long input = scanner.nextLong();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static BigDecimal nextBigDecimal() {
+        ensureScanner();
+        BigDecimal input = scanner.nextBigDecimal();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static BigInteger nextBigInteger() {
+        ensureScanner();
+        BigInteger input = scanner.nextBigInteger();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static byte nextByte() {
+        ensureScanner();
+        byte input = scanner.nextByte();
+        scanner.nextLine();
+        return input;
+    }
+}
